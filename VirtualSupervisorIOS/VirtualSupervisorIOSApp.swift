79e645965508f29f56b38a109fffc5108eff589d
Rodion Hladchenko <r.hladchenko@pnn.in.ua>
2022-02-09 15:54:21 +0200

    Initial Commit

---
+++ VirtualSupervisorIOS/VirtualSupervisorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualSupervisorIOSApp.swift
+//  VirtualSupervisorIOS
+//
+//  Created by Rodion Hladchenko on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VirtualSupervisorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
